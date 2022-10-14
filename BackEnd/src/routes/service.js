8ba5444346733012184159ab68ac86e6981f63db
SON <wjdgh2777@naver.com>
2022-10-15 01:01:18 +0900

    Comment: 주석 제거

--- BackEnd/src/routes/service.js
+++ BackEnd/src/routes/service.js
@@ -5,8 +5,8 @@ var fs = require("fs");
 
 exports.home = (req, res) => {
     fs.readFile("test.html", (error, data) => {
-        res.writeHead(200, { "Content-Type": "text/html" }); // 응답 헤더에 대한 정보를 기록하는 메서드, 응답의 콘텐츠 형식이 HTML
-        res.end(data); // 주로 서버가 작동을 안하거나 오류가 있을 경우, 특정 문구를 나타내고 응답을 종료하고자 할 때 사용
+        res.writeHead(200, { "Content-Type": "text/html" });
+        res.end(data);
     });
 };
 
